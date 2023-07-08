f465d738963fd0a341847ea2dbf49b7129d17eb7
Mrchlldev <137673915+Mrchlldev@users.noreply.github.com>
2023-07-08 08:24:25 +0700

    Update Loader.php

--- src/BlockHorizons/Fireworks/Loader.php
+++ src/BlockHorizons/Fireworks/Loader.php
@@ -10,12 +10,14 @@ use pocketmine\data\bedrock\EntityLegacyIds;
 use pocketmine\entity\EntityDataHelper;
 use pocketmine\entity\EntityFactory;
 use pocketmine\item\VanillaItems;
+use pocketmine\item\StringToItemParser;
 use pocketmine\item\ItemIdentifier;
 use pocketmine\item\ItemTypeIds;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
 use pocketmine\plugin\PluginBase;
 use pocketmine\world\World;
+use pocketmine\network\mcpe\convert\TypeConverter;
 
 class Loader extends PluginBase
 {
@@ -23,8 +25,8 @@ class Loader extends PluginBase
 	public function onEnable(): void
 	{
 		VanillaItems::register(new Fireworks(new ItemIdentifier(ItemTypeIds::FIREWORKS, 0), "Fireworks"), true);
-		EntityFactory::getInstance()->register(FireworksRocket::class, static function (World $world, CompoundTag $nbt): FireworksRocket {
-			return new FireworksRocket(EntityDataHelper::parseLocation($nbt, $world), ItemFactory::getInstance()->get(ItemIds::FIREWORKS));
+		EntityFactory::getInstance()->register(FireworksRocket::class, static function (World $world, CompoundTag $nbt, TypeConverter $typeConverter): FireworksRocket {
+			return new FireworksRocket(EntityDataHelper::parseLocation($nbt, $world), StringToItemParser::getInstance()->parse(ItemTypeIds::FIREWORKS));
 		}, ["FireworksRocket", EntityIds::FIREWORKS_ROCKET], EntityLegacyIds::FIREWORKS_ROCKET);
 	}
 }
